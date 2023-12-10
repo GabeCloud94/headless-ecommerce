ce8bb989f800fbdc924ed86d9e571f72634d0368
gabecloud94 <gabecloud94@gmail.com>
2023-12-09 17:25:22 -0700

    Hero image mobile spacing

--- components/hero-slider.tsx
+++ components/hero-slider.tsx
@@ -15,7 +15,7 @@ export async function HeroSlider() {
   if (!homepageItems?.length) return null;
 
   return (
-    <div className='md:max-w-7xl md:flex md:flex-row md:justify-center md:items-center gap-4 md:mx-auto border rounded-lg p-6 mb-6 bg-secondary'>
+    <div className='xl:max-w-7xl md:flex md:flex-row md:justify-center md:items-center gap-4 xl:mx-auto border rounded-lg p-6 mb-6 bg-secondary mx-4'>
       <div className='flex flex-col gap-4 text-center mb-4 md:mb-0'>
         <h1 className='text-5xl'>{SITE_NAME}</h1>
         <p className='text-2xl'>Be the First to Check Out Our New Arrivals!</p>
