e0f59b517a71879c608ab3a03cc805b1f6884782
gabecloud94 <gabecloud94@gmail.com>
2023-12-11 11:34:29 -0700

    Update label component styling

--- components/label.tsx
+++ components/label.tsx
@@ -14,12 +14,12 @@ const Label = ({
 }) => {
   return (
     <div
-      className={clsx('absolute bottom-0 left-0 flex w-full px-4 pb-4 @container/label', {
+      className={clsx('absolute bottom-0 left-0 flex flex-col md:flex-row w-full px-1 pb-4 @container/label', {
         'lg:px-20 lg:pb-[35%]': position === 'center'
       })}
     >
       <div className="flex items-center rounded-full border border-border bg-foreground/70 p-1 text-xs font-semibold text-background backdrop-blur-lg">
-        <h3 className="mr-4 line-clamp-2 flex-grow pl-2 text-base">{title}</h3>
+        <h3 className="mr-1 md:mr-4 line-clamp-1 flex-grow pl-2 text-sm md:text-base">{title}</h3>
         <Price
           className="flex-none rounded-full p-2 text-foreground"
           amount={amount}
