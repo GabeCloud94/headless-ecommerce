ddf1acaf153aaa402c56db37aea124920a5ca86c
gabecloud94 <gabecloud94@gmail.com>
2023-12-11 12:23:20 -0700

    Update cart styles and button colors

--- components/cart/modal.tsx
+++ components/cart/modal.tsx
@@ -176,7 +176,7 @@ export default function CartModal({ cart }: { cart: Cart | undefined }) {
                   </div>
                   <Link
                     href={cart.checkoutUrl}
-                    className="block w-full rounded-full bg-foreground dark:bg-background p-3 text-center text-sm font-medium text-background dark:text-foreground opacity-90 hover:opacity-100"
+                    className="block w-full rounded-full bg-primary p-3 text-center text-sm font-medium text-secondary opacity-90 hover:opacity-100"
                   >
                     Proceed to Checkout
                   </Link>
